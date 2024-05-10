41c93ef204388540bdd84c25b2d49e307505aa5c
Rabbani <59020772+rabbanext@users.noreply.github.com>
2024-05-10 19:25:47 +0700

    update email verify alert

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -7,26 +7,25 @@
 </div>
 @if (session('resent'))
 <div class="alert alert-success" role="alert">
-    {{ __('A fresh verification link has been sent to your email address.') }}
+    A fresh verification link has been sent to <strong>{{ Auth::user()->email }}</strong>
 </div>
 @endif
 <div class="section-content">
     <h3 class="text-center">Verify Your Email Address</h3>
 
     <p class="text-center">
-        {{ __('Before proceeding, please check your email for a verification link.') }}
+        Before proceeding, please check your email for a verification link.
     </p>
 
     <form class="text-center my-5" method="POST" action="{{ route('verification.resend') }}">
         @csrf
-        <button type="submit" class="btn btn-primary">{{ __('click here to
-            request another') }}</button>
+        <button type="submit" class="btn btn-primary">click here to request another</button>
     </form>
     <p class="text-center p-0">
         <a class="text-danger" href="{{ route('logout') }}"
             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="bx bx-power-off"></i>
-            <span class="align-middle">{{ __('Logout') }}</span>
+            <span class="align-middle">Logout</span>
         </a>
     </p>
     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
