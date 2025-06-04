c8e25b159044a70bf0153c06ecb82fdde3dbd170
Rinaldi <rinaldiamfine@gmail.com>
2025-06-04 15:06:38 +0700

    Update the terms

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -8,17 +8,115 @@
 		<section id="about" class="about">
 			<div class="container">
 				<div class="content section-content">
-					<h3 class="fst-italic">
+					<h3 class="fst-italic mb-4">
 						Syarat dan Ketentuan Lomba Hackathon Bank Indonesia 2025
 					</h3>
+					<h5 class="pt-4">I. Pendahuluan</h5>
 					<p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+						BI-OJK Hackathon 2025 (<b>“Hackathon”</b>), sebagai bagian dari rangkaian acara Festival Ekonomi Digital Indonesia (<b>“FEKDI”</b>) 2025 dan Indonesia Fintech Summit & Expo (<b>“IFSE”</b>) 2025 yang diselenggarakan oleh Bank Indonesia dan OJK (<b>“Penyelenggara”</b>), merupakan ajang kompetisi intelektual yang diikuti oleh industri, akademisi, dan/atau individu selaku inovator, pengembang, dan pemikir kreatif (<b>“Peserta”</b>) untuk memanfaatkan teknologi khususnya artificial intelligence atau machine learning (<b>“AI/ML”</b>) dan block chain dalam menyelesaikan berbagai permasalahan. Hackathon memberikan kesempatan bagi setiap individu untuk menunjukkan kemampuan dalam mengatasi tantangan, mengembangkan solusi, dan bekerja sama dalam tim. Selain itu, Hackathon juga menjadi platform pembelajaran, pertumbuhan, dan jaringan bagi para pihak dengan beragam latar belakang industri. Melalui kolaborasi dan inovasi, kami yakin bahwa kita mampu memanfaatkan AI/ML dan block chain untuk membantu mengatasi berbagai tantangan yang dihadapi oleh dunia saat ini.
 					</p>
+
+					<h5 class="pt-4">II. Tujuan</h5>
 					<p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+						Penyelenggaraan Hackathon bertujuan untuk mendorong pengembangan pemanfaatan AI/ML dan block chain oleh industri, akademisi, dan individu guna mendukung perluasan ekonomi dan keuangan digital. Pemanfaatan AI/ML dan block chain tidak hanya diharapkan dapat menyelesaikan permasalahan yang disediakan namun juga memiliki potensi model bisnis yang mampu diimplementasikan dan dikembangkan ke depan. 
 					</p>
+
+					<h5 class="pt-4">III. Peserta</h5>
 					<p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+						Hackathon dirancang untuk para inovator, pengembang, dan pemikir kreatif serta profesional atau perorangan yang berpengalaman di bidang data, AI/ML, dan block chain yang ingin menunjukkan keahlian mereka dan berkontribusi dalam pengembangan solusi inovatif menuju integrasi ekonomi digital dan keuangan Indonesia. 
+					</p>
+					<h6>Persyaratan Umum Peserta</h6>
+					<ol>
+						<li>Peserta merupakan Warga Negara Indonesia.</li>
+						<li>Peserta dapat membentuk tim dengan jumlah maksimal 4 anggota.</li>
+						<li>Peserta bukan merupakan pegawai dari organisasi yang tergabung sebagai panitia atau penyelenggara.</li>
+					</ol>
+					<h6>Persyaratan Khusus Peserta</h6>
+					<p>Kategori mahasiswa:  Seluruh anggota tim merupakan mahasiswa aktif program  S1 dan S2 dari perguruan tinggi di Indonesia maupun luar negeri, dibuktikan dengan kartu tanda mahasiswa (KTM) atau surat keterangan aktif kuliah.</p>
+					<p>Kategori Profesional: Terbuka bagi tim yang terdiri dari lulusan perguruan tinggi, pelaku industri, startup, dosen, peneliti, ASN, freelancer maupun pekerja profesional lainnya.</p>
+					
+					<h5 class="pt-4">IV. Tata Cara Pendaftaran</h5>
+					<p>
+						Tahapan Registrasi dengan mengunjungi situs <a class="fst-italic" href="https://hackathon.fekdi.co.id">https://hackathon.fekdi.co.id</a>, lalu klik “Register”, dan lengkapi informasi untuk membuat akun. Selanjutnya, lengkapi form yang telah disediakan dengan mengisi informasi, sebagai berikut:
+					</p>
+					<ol>
+						<li>Nama tim</li>
+						<li>Asal instansi/organisasi/perusahaan</li>
+						<li>Informasi anggota tim</li>
+						<li>Upload proposal yang sesuai dengan format panduan proposal</li>
+					</ol>
+					<p>Jika telah ter-submit akan dikirimkan email sebagai informasi data telah lengkap.</p>
+
+					<h5 class="pt-4">V. MENTORING DAN KARANTINA</h5>
+					<h6>1. Mentoring Pada Tahapan Prototyping</h6>
+					<ol type="a">
+						<li>Peserta yang lolos menjadi 10 Finalis wajib mengikuti pelaksanaan mentoring yang dijadwalkan oleh Penyelenggara.</li>
+						<li>Mentoring merupakan kegiatan yang bertujuan untuk membantu tim peserta mencapai hasil terbaik dengan memonitor dan memberikan bimbingan konsultasi atas progress pembuatan prototype. Proses mentoring dilakukan oleh mentor berpengalaman berlatar belakang akademisi, teknologi dan bisnis yang disediakan oleh penyelenggara.</li>
+						<li>Mentoring dihadiri Peserta minimal 1 (satu) kali pertemuan selama periode mentoring dengan sekurang-kurangnya 1 (satu) anggota tim Peserta.</li>
+						<li>Peserta atau tim yang tidak menghadiri sesi mentoring minimal 1 kali, dapat dikenai sanksi, termasuk penurunan nilai atau diskualifikasi, menyesuaikan dengan kebijakan penyelenggara</li>
+					</ol>
+					<h6>2. Karantina</h6>
+					<ol type="a">
+						<li>Pada tahapan Karantina, Penyelenggara menyampaikan surat undangan kepada Peserta yang terpilih untuk menjalani karantina yang berlokasi di Jakarta.</li>
+						<li>Setiap anggota tim dari Peserta yang menerima undangan karantina wajib hadir dan mengikuti seluruh rangkaian karantina sesuai dengan periode jangka waktu yang ditentukan.</li>
+						<li>Dalam hal terdapat sebagian anggota tim Peserta yang tidak dapat hadir dalam karantina, maka Peserta wajib menyampaikan alasan ketidakhadiran kepada Penyelenggara untuk menjadi pertimbangan bagi Penyelenggara.</li>
+						<li>
+							Dalam penyelenggaraan karantina, Penyelenggara menyediakan:
+							<ol>
+								<li>transportasi: tiket pesawat, kereta api, atau bus domestik (di seluruh wilayah Indonesia) sesuai dengan lokasi anggota tim Peserta berupa tiket keberangkatan dan kepulangan dari/ke Jakarta sebagai lokasi karantina.</li>
+								<li>akomodasi: hotel dan konsumsi selama periode karantina.</li>
+							</ol>
+						</li>
+						<li>Setiap anggota tim Peserta bertanggung jawab masing-masing terhadap kehadirannya dalam karantina termasuk namun tidak terbatas antara lain pada izin yang diperlukan dari institusi/lembaga/perusahaan dimana anggota tim Peserta bernaung atau bekerja atau tanggung jawab kepada pihak lain sehubungan dengan kehadiran anggota tim Peserta dalam karantina.</li>
+						<li>Penyelenggara berwenang mengenakan sanksi termasuk mempertimbangkan hasil penilaian Peserta atau mengeluarkan Peserta dari keikutsertaaanya dalam Hackathon apabila seluruh anggota tim Peserta gagal menghadiri dan mengikuti karantina sesuai dengan jangka waktu yang ditentukan secara sempurna.</li>
+					</ol>
+
+					<h5 class="pt-4">VI. Kepemilikan Intelektual</h5>
+					<ol>
+						<li>Setiap peserta akan tetap berhak atas setiap hak atas hak kekayaan intelektual yang dihasilkan dari keikutsertaan Peserta dalam Hackathon sesuai dengan peraturan perundang-undangan.</li>
+						<li>Peserta menyetujui bahwa Penyelenggara dapat menggunakan dan mempublikasikan proyek peserta untuk kepentingan Hackathon antara lain promosi dan komunikasi, termasuk event Hackathon dalam rangkaian acara FEKDI x IFSE yang diselenggarakan Bank Indonesia, dan pengembangan teknologi AI/ML dan blockchain dengan tetap memperhatikan ketentuan peraturan perundang-undangan.</li>
+						<li>Peserta yang terpilih menjadi finalis memberikan kesempatan pertama kepada Penyelenggara untuk memanfaatkan atau mengembangkan prototype yang dihasilkan selama keikusertaan dalam Hackathon sebelum ditawarkan kepada pihak lain yang bersifat tidak mengikat kecuali berdasarkan kesepakatan kedua belah pihak.</li>
+					</ol>
+					
+					<h5 class="pt-4">VII. Kewajiban Peserta</h5>
+					<ol>
+						<li>Peserta wajib mengikuti seluruh rangkaian kegiatan Hackathon yang ditetapkan oleh Penyelenggara.</li>
+						<li>Peserta bertanggung jawab dan wajib mematuhi seluruh ketentuan dalam Hackathon dan bertindak dengan itikad baik, serta menjunjung tinggi norma dan etika yang berlaku selama berpartisipasi dalam Hackathon.</li>
+						<li>
+							Peserta bertanggung jawab terhadap setiap materi, konten, data, dan informasi dari proposal/proyek/solusi yang dihasilkan oleh masing-masing Peserta dan dengan demikian membebaskan Penyelenggara dari setiap tuntuntan, permintaan, tindakan, proses hukum kewajiban (termasuk kewajiban hukum dan kewajiban kepada pihak ketiga), sanksi, dan biaya (termasuk namun tidak terbatas pada biaya hukum secara penuh), hadiah, kerugian dan/atau biaya, yang timbul atau berkaitan dengan:
+							<ol type="a">
+								<li>Penggunaan platform pengembangan;</li>
+								<li>Partisipasi Peserta dalam Hackathon, termasuk kehadiran dalam karantina;</li>
+								<li>Pengajuan Solusi Peserta;</li>
+								<li>Pelanggaran Peserta terhadap setiap Syarat dan Ketentuan ini;</li>
+								<li>Pelanggaran Peserta terhadap hak-hak orang atau entitas lain, termasuk hak kekayaan intelektual; atau</li>
+								<li>Pelanggaran Peserta terhadap setiap persyaratan, kewajiban, atau hukum yang berlaku.</li>
+							</ol>
+						</li>
+						<li>Penyelenggara berwenang untuk mengeluarkan Peserta dari keikutsertaannya (diskualifikasi) dalam Hackathon apabila Peserta melanggar ketentuan Hackathon, melanggar peraturan perundang-undangan, melakukan kecurangan, plagiarisme, dan tindakan lain yang tidak menunjukkan itikad baik atau pelanggaran etika lainnya.</li>
+					</ol>
+
+					<h5 class="pt-4">VIII. Perlindungan Data</h5>
+					<ol>
+						<li>Data pribadi peserta akan diproses dan dikelola sesuai dengan kepentingan penyelenggaraan Hackathon, termasuk kegiatan terkait promosi dan komunikasi, rangkaian Hackathon termasuk FEKDI, mentoring, dan kegiatan lainnya terkait Hackathon dengan memperhatikan peraturan perundang-undangan terkait perlindungan data pribadi.</li>
+						<li>Peserta bertanggung jawab terhadap perlindungan dan kerahasiaan terhadap data yang digunakan oleh peserta dalam Hackathon termasuk mematuhi ketentuan yang tertuang dalam Surat Pernyataan Menjaga Kerahasiaan Data (Non-disclosure Agreement), bagi Peserta finalis.</li>
+					</ol>
+
+					<h5 class="pt-4">IX. Keputusan Penyelenggara</h5>
+					<p>
+						Keputusan Penyelenggara dalam Hackathon, termasuk tetapi tidak terbatas pada pendaftaran, evaluasi proposal, tahapan seleksi, penilaian, dan penentuan pemenang bersifat mutlak dan tidak dapat diganggu gugat.
+					</p>
+
+					<h5 class="pt-4">X. Penyelesaian Perselisihan</h5>
+					<ol>
+						<li>Penyelenggara, Peserta, dan penyelenggaraan Hackathon tuntuk pada hukum yang berlaku di Indonesia.</li>
+						<li>Setiap perselisihan akan diselesaikan oleh Peserta dan Penyelenggara secara musyawarah untuk mencapai mufakat.</li>
+						<li>Jika mufakat tidak dapat dicapai, setiap perselisihan yang timbul dari atau sehubungan dengan syarat dan ketentuan ini mengenai hubungan antara Peserta dan Bank Indonesia akan dirujuk dan akhirnya diselesaikan berdasarkan Aturan Administratif dan Prosedural Arbitrase Badan Arbitrase Nasional Indonesia (BANI). Jumlah arbitrator adalah 1 (satu). Ketua BANI akan menunjuk arbitrator.</li>
+					</ol>
+
+					<h5 class="pt-4">XI. Perubahan</h5>
+					<p>
+					Penyelenggara dapat sewaktu-waktu mengubah atau memperbarui Syarat dan Ketentuan. Setiap perubahan atau pembaruan dipublikasikan dalam situs web BI-OJK Hackathon 2025.
 					</p>
 				</div>
 			</div>
